041c3303096303b99aa49ca984547af1801c537f
nsant <nsantantonio@gmail.com>
2025-02-17 07:59:19 -0500

    add notes, clean up old code, add local path, QTL table

--- dataAnalysis.R
+++ dataAnalysis.R
@@ -1,5 +1,20 @@
-load("/home/nsant/Dropbox/reproducibleScience/SRWobs_2022_WARVA_forPMgwas.RData")
+# load rrblup library
+# install.packages("rrBLUP") # install if not done, only needs to be run one time!
+library(rrBLUP)
 
+# source our functions from a script (even better make it a package!)
+source("myfunctions.R")
+
+# get path to data. 
+local <- FALSE
+if(local){
+	path <- "/home/nsant/Dropbox/reproducibleScience/" 
+} else {
+	path <- "./" 
+}
+load(paste0(path, "SRWobs_2022_WARVA_forPMgwas.RData"))
+
+# make design and incidence matrices for model fit
 X <- model.matrix(~Block, data = obsWR)
 Z <- model.matrix(~Line -1, data = obsWR)
 colnames(Z) <- gsub("^Line", "", colnames(Z))
@@ -10,10 +25,9 @@ if (all(colnames(Z) == rownames(Mwr))) {
 	stop("colnames of Z dont match row names of M!")
 }
 
-################ 
-# for plotting #
-################
-
+####################################################
+# get cumulative positions of markers for plotting #
+####################################################
 
 pos <- snpInfo
 pos <- cbind(pos, cumpos = NA)
@@ -28,9 +42,9 @@ for(i in unique(pos[,"chrom"])){
 labPos <- tapply(pos$cumpos, pos$chrom, function(x) (max(x) - min(x)) / 2 + min(x))
 
 
-###############
-# niave model #
-###############
+#############################
+# niave GWAS model with OLS #
+#############################
 
 trait <- "PowderyMildew"
 
@@ -44,44 +58,34 @@ for(i in colnames(ZM)){
 }
 
 
-
-# make function to make manhattan plot
+# combine snp positions with pvalues
 pvals <- cbind(snpInfo, pval = pval)
 
-source("myfunctions.R")
-
+# user defined function to make a manhattan plot
 manhattan(pvals) # use user defined colors in data.frame 'pvals' 
 manhattan(pvals[!names(pvals) %in% "col"]) # default colors from function
-
-# # if you are feeling particularly patriotic
-manhattan(pvals[!names(pvals) %in% "col"], cols = c("firebrick", "royalblue4"))
 
 # # just in case you want to be extra garish
 manhattan(pvals[!names(pvals) %in% "col"], cols = c("#861F41", "#E87722", "#75787b"))
 
 # make it a pdf
 pdf("naiveGWAS.pdf", width = 14)
-manhattan(pvals)
+manhattan(pvals[!names(pvals) %in% "col"])
 dev.off()
 
 
-# this is what is different!
 
+##################################################
+# GWAS model correcting for population structure #
+##################################################
 
-
-
-###############
-# GWAS model #
-###############
-library(rrBLUP)
 ZMt <- as.data.frame(t(ZM)) # to make 'geno' for GWAS()
 colnames(ZMt) <- obsWRzm$Line 
 
 ZKZt <- Z %*% Kwr %*% t(Z) # expand K onto data structure
 rownames(ZKZt) <- colnames(ZKZt) <- obsWRzm$Line
 
-# note this is super repetative and generally unneccessary. We were just rushing to analyze multiple traits before lab meeting. 
-# really, it should be looped over traits, and the pvals saved, etc. 
+# loop over traits, pvals saved
 traits <- c("PowderyMildew", "GrainYield")#, "TestWeight", "HeadingDate", "Height")
 # triats <- traits[1]
 resultList <- list()
@@ -91,54 +95,27 @@ for (i in traits){
 	resultList[[i]] <- GWAS(pheno = pheno, geno = geno, fixed = c("Block"), K = ZKZt, plot = FALSE)
 }
 
+
+
+# plot results
 pdf("p3dGWASmanhattan.pdf")
-manhattan(resultList[[1]])
-manhattan(resultList[[2]])
-# manhattan(resultList[[3]])
-# manhattan(resultList[[4]])
+	manhattan(resultList[[1]], main = traits[1])
+	manhattan(resultList[[2]], main = traits[2])
 dev.off()
 
-# pheno <- obsWRzm[c("Line", "Block", "GrainYield")]
-# ZMt <- as.data.frame(t(ZM))
-# colnames(ZMt) <- pheno$Line
-# geno <- cbind(snpInfo[c("rs", "chrom", "pos")], ZMt)
-# geno[1:10, 1:20]
-# ZKZt <- Z %*% Kwr %*% t(Z) 
+# add function to make qq plot in class. 
 
-# rownames(ZKZt) <- colnames(ZKZt) <- pheno$Line
-# # all(pheno$Line %in% colnames(ZKZt))
-# rrgwas <- GWAS(pheno = pheno, geno = geno, fixed = c("Block"), K = ZKZt)
+# print table of markers with significant effects for trait 1
+alpha <- 0.05
+bonferroni <- alpha / ncol(Mwr)
+QTL <- resultList[[1]][resultList[[1]][,4] >= -log10(bonferroni),]
+names(QTL) <- c("SNP", "Chromosome", "Position", "-log(P-value)")
+print(QTL)
 
 
-# pheno <- obsWRzm[c("Line", "Block", "TestWeight")]
-# ZMt <- as.data.frame(t(ZM))
-# colnames(ZMt) <- pheno$Line
-# geno <- cbind(snpInfo[c("rs", "chrom", "pos")], ZMt)
-# geno[1:10, 1:20]
-# ZKZt <- Z %*% Kwr %*% t(Z) 
+# below will make a latex table for a latex document
 
-# rownames(ZKZt) <- colnames(ZKZt) <- pheno$Line
-# # all(pheno$Line %in% colnames(ZKZt))
-# rrgwas <- GWAS(pheno = pheno, geno = geno, fixed = c("Block"), K = ZKZt)
-
-# pheno <- obsWRzm[c("Line", "Block", "HeadingDate")]
-# ZMt <- as.data.frame(t(ZM))
-# colnames(ZMt) <- pheno$Line
-# geno <- cbind(snpInfo[c("rs", "chrom", "pos")], ZMt)
-# geno[1:10, 1:20]
-# ZKZt <- Z %*% Kwr %*% t(Z) 
-
-# rownames(ZKZt) <- colnames(ZKZt) <- pheno$Line
-# # all(pheno$Line %in% colnames(ZKZt))
-# rrgwas <- GWAS(pheno = pheno, geno = geno, fixed = c("Block"), K = ZKZt)
-
-# pheno <- obsWRzm[c("Line", "Block", "Height")]
-# ZMt <- as.data.frame(t(ZM))
-# colnames(ZMt) <- pheno$Line
-# geno <- cbind(snpInfo[c("rs", "chrom", "pos")], ZMt)
-# geno[1:10, 1:20]
-# ZKZt <- Z %*% Kwr %*% t(Z) 
-
-# rownames(ZKZt) <- colnames(ZKZt) <- pheno$Line
-# # all(pheno$Line %in% colnames(ZKZt))
-# rrgwas <- GWAS(pheno = pheno, geno = geno, fixed = c("Block"), K = ZKZt)
+# library(devtools) # if you dont have devtools installed already.
+# install_github("summaryTools",username="nsantantonio")
+# library(summaryTools)
+# xtable2(QTL, con = "qtlTable.tex", include.rownames = FALSE)
